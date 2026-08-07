db07573fa1ab82653b2c9608b88f10eb4f1657d2
musicallman1 <hakeem@bedesigned.co>
2026-08-07 01:48:21 -0400

    Refactor SQL for chi-squared hypothesis test

--- queries/hypothesis_test/chi_squared.sql
+++ queries/hypothesis_test/chi_squared.sql
@@ -1,37 +1,42 @@
-WITH categorized AS 
-(SELECT
-  CASE 
-    WHEN BusinessAge = 'Startup, Loan Funds will Open Business'THEN 'Startup'
-    ELSE 'Non-Startup'
-  END AS OnRows,
-  CASE 
-    WHEN CAST(NaicsCode AS STRING) LIKE '7225%'THEN 'Restaurant'
-    ELSE 'Non-restaurant'
-  END AS OnCols
-FROM yelp-analysis-503216.Philly_yelp.sba_loans
-), 
+WITH categorized AS (
+  SELECT
+    CASE
+      WHEN BusinessAge = 'Startup, Loan Funds will Open Business' THEN 'Startup'
+      ELSE 'Non-Startup'
+    END AS business_age_group,
+    CASE
+      WHEN STARTS_WITH(CAST(NaicsCode AS STRING), '7225') THEN 'Restaurant'
+      ELSE 'Non-restaurant'
+    END AS industry_type
+  FROM `yelp-analysis-503216.Philly_yelp.sba_loans`
+),
 
-ObservedCombinationCTE AS 
-(SELECT 
-  Onrows,
-  Oncols,
-  COUNT(*) AS ObservedCombination
-FROM categorized
-GROUP BY Onrows, Oncols),
+observed AS (
+  SELECT
+    business_age_group,
+    industry_type,
+    COUNT(*) AS observed_count
+  FROM categorized
+  GROUP BY business_age_group, industry_type
+),
 
-ExpectedCombinationCTE AS
-(SELECT
-  Onrows, Oncols, ObservedCombination, 
-  SUM(ObservedCombination) OVER (PARTITION BY OnRows) AS ObservedOnRows, 
-  SUM(ObservedCombination) OVER (PARTITION BY OnCols) AS ObservedOnCols, 
-  SUM(ObservedCombination) OVER () AS ObservedTotal,
-  (SUM(1.0 * ObservedCombination) OVER (PARTITION BY OnRows)
-  * SUM(1.0 * ObservedCombination) OVER (PARTITION BY OnCols)
-  / SUM(1.0 * ObservedCombination) OVER ()) AS ExpectedCombination
-FROM ObservedCombinationCTE
+expected AS (
+  SELECT
+    business_age_group,
+    industry_type,
+    observed_count,
+    SUM(observed_count) OVER (PARTITION BY business_age_group) AS row_total,
+    SUM(observed_count) OVER (PARTITION BY industry_type) AS col_total,
+    SUM(observed_count) OVER () AS grand_total,
+    SAFE_DIVIDE(
+      SUM(observed_count) OVER (PARTITION BY business_age_group)
+        * SUM(observed_count) OVER (PARTITION BY industry_type),
+      SUM(observed_count) OVER ()
+    ) AS expected_count
+  FROM observed
 )
 
 SELECT
-  SUM(POWER(ObservedCombination - ExpectedCombination, 2) / ExpectedCombination) AS ChiSquared,
-  (COUNT(DISTINCT OnRows) - 1) * (COUNT(DISTINCT OnCols) - 1) AS DegreesOfFreedom
-FROM ExpectedCombinationCTE
+  SUM(POWER(observed_count - expected_count, 2) / expected_count) AS chi_squared,
+  (COUNT(DISTINCT business_age_group) - 1) * (COUNT(DISTINCT industry_type) - 1) AS degrees_of_freedom
+FROM expected
